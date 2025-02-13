cf69c9c3e707307b305b1e02444990478e7a2452
Immanuel Mruk <immanuelmruk@gmail.com>
2025-02-13 11:49:22 +0100

    Load config from .env

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "!@?Sup€rS€cretK€y?@!")
     CORS_ORIGINS = os.getenv("CORS_ORIGINS", "http://localhost:5000").split(",")
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "!@?Sup€rS€cretK€y?@!")
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', SECRET_KEY)
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///database.db")
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS", "False").lower() == "true"
 
     MAX_LOGIN_ATTEMPTS = int(os.getenv("MAX_LOGIN_ATTEMPTS", 5))
